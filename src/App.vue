a1ef5ac6af4bd3b1c187cf3f4880a702c2aed558
HL <821890952@qq.com>
2023-05-19 22:28:15 +0800

    水果案例

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
-  <div ref="myDiv">使用组件</div>
-  <Form ref="myForm" title="登录表单" @myEmit="handleMyEmit" />
-  <hr />
-  <Form ref="myForm" title="注册表单" />
+  <div>
+    <input type="text" v-model="inputText" />
+  </div>
+  <button @click="add">添加</button>
+  <div>
+    <ul>
+      <li v-for="item in itemList" :key="item.id" style="border: 1px solid black" @click="removeItem(item)">
+        {{ item.text }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
-  import { onMounted, ref } from "vue";
-  import Form from "./components/Form.vue";
-  onMounted(() => {
-    console.log(Form);
-    console.log(myDiv.value);
-    console.log(myForm.value);
-    console.log("获取子组件数据", myForm.value.msg.name);
-  });
-  const myDiv = ref(null);
-  const myForm = ref(null);
-  const handleMyEmit = (data) => {
-    console.log("子传父数据", data);
+  import { ref } from "vue";
+
+  const add = () => {
+    if (inputText.value !== "") {
+      const newItem = { id: Date.now(), text: inputText.value };
+      if (!itemList.value.some((item) => item.text === newItem.text)) {
+        itemList.value.push(newItem);
+      }
+      inputText.value = "";
+    }
   };
+  const removeItem = (item) => {
+    const index = itemList.value.findIndex((element) => element === item);
+    if (index !== -1) {
+      itemList.value.splice(index, 1);
+    }
+  };
+  const inputText = ref("");
+  const itemList = ref([]);
 </script>
 <style lang="scss" scoped></style>
---
+++ src/演示代码/父传子,子传父演示.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="myDiv">使用组件</div>
+  <Form ref="myForm" title="登录表单" @myEmit="handleMyEmit" />
+  <hr />
+  <Form ref="myForm" title="注册表单" />
+</template>
+
+<script setup>
+  import { onMounted, ref } from "vue";
+  import Form from "../components/Form.vue";
+  onMounted(() => {
+    console.log(Form);
+    console.log(myDiv.value);
+    console.log(myForm.value);
+    console.log("获取子组件数据", myForm.value.msg.name);
+  });
+  const myDiv = ref(null);
+  const myForm = ref(null);
+  const handleMyEmit = (data) => {
+    console.log("子传父数据", data);
+  };
+</script>
+<style lang="scss" scoped></style>
