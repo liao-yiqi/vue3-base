6fca7bbb9871472e34be10b9e5a27c2a354f6c10
komorebiHl <821890952@qq.com>
2023-05-19 11:23:22 +0800

    ref基本使用

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <button @click="toggle">显示隐藏图片</button>
-  <img v-show="show" alt="Vue logo" src="./assets/logo.png" />
-  <hr />
-  计数器：{{ count }} <button @click="increment">累加</button>
+  {{ student.name }}的薪资{{ student.salary }}
+  <button @click="add">增加</button>
 </template>
+
 <script>
   // ref 就是一个组合式API
   import { ref } from "vue";
   export default {
     setup() {
-      // 显示隐藏
-      const show = ref(true);
-      const toggle = () => {
-        show.value = !show.value;
+      const student = ref({ name: "李哥", salary: 15000 });
+      const add = () => {
+        student.value.salary++;
       };
-      // 计数器
-      const count = ref(0);
-      const increment = () => {
-        count.value++;
-      };
-
-      return { show, toggle, count, increment };
+      return { student, add };
     },
   };
 </script>
