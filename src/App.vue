f31ab600e5d822342722c9e9d6a352ad8e937619
komorebiHl <821890952@qq.com>
2023-05-19 10:53:15 +0800

    vue3初体验

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="toggle">显示隐藏图片</button>
+  <img v-show="show" alt="Vue logo" src="./assets/logo.png" />
+  <hr />
+  计数器：{{ count }} <button @click="increment">累加</button>
+</template>
+<script>
+  // ref 就是一个组合式API
+  import { ref } from "vue";
+  export default {
+    setup() {
+      // 显示隐藏
+      const show = ref(true);
+      const toggle = () => {
+        show.value = !show.value;
+      };
+      // 计数器
+      const count = ref(0);
+      const increment = () => {
+        count.value++;
+      };
+
+      return { show, toggle, count, increment };
+    },
+  };
+</script>
