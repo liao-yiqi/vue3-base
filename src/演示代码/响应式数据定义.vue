313317587a8e4ee323620af1ededc5dfae6cfb2f
komorebiHl <821890952@qq.com>
2023-05-19 14:52:09 +0800

    响应式数据定义

---
+++ src/演示代码/响应式数据定义.vue
@@ -0,0 +1,13 @@
+<template>
+  {{ student.name }}的薪资{{ student.salary }}
+  <button @click="add">增加</button>
+</template>
+
+<script setup>
+  // ref 就是一个组合式API
+  import { ref } from "vue";
+  const student = ref({ name: "李哥", salary: 15000 });
+  const add = () => {
+    student.value.salary++;
+  };
+</script>
