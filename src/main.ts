ad0d09bc81c1e13079a186a474d8832d23c77bd2
komorebiHl <821890952@qq.com>
2023-05-25 09:47:54 +0800

    引入Pinia

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 // import "./style.css";
 import "./styles/index.css";
 import App from "./App.vue";
-
-createApp(App).mount("#app");
+import { createPinia } from "pinia";
+const Pinia = createPinia;
+const app = createApp(App);
+app.use(Pinia);
+app.mount("#app");
